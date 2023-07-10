22cd7bdd214e5ccdfd51e92a0c37f0f8bc5e9aa3
SDeshpande <sarang.deshpande@trakcel.com>
2023-07-10 18:07:05 +0530

    Deleting unnecessary files and code cleanup.

--- force-app/main/default/lwc/fixedColumnedDatatable/fixedColumnedDatatable.js
+++ force-app/main/default/lwc/fixedColumnedDatatable/fixedColumnedDatatable.js
@@ -1,29 +1,29 @@
 import { LightningElement } from 'lwc';
 
 const dataFirstTable = [
-    {id: 1, name: 'Robert Downy Sarang.', email: 'ironmandad@starc.com'},
-    {id: 2, name: 'Robert Downy Jr.', email: 'ironman@starc.com'},
-    {id: 3, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com'},
-    {id: 4, name: 'Robert Downy Jr.', email: 'ironman@starc.com'},
-    {id: 5, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com'},
-    {id: 6, name: 'Robert Downy Jr.', email: 'ironman@starc.com'},
-    {id: 7, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com'},
-    {id: 8, name: 'Robert Downy Jr.', email: 'ironman@starc.com'},
+    { id: 1, name: 'Robert Downy Sarang.', email: 'ironmandad@starc.com' },
+    { id: 2, name: 'Robert Downy Jr.', email: 'ironman@starc.com' },
+    { id: 3, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com' },
+    { id: 4, name: 'Robert Downy Jr.', email: 'ironman@starc.com' },
+    { id: 5, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com' },
+    { id: 6, name: 'Robert Downy Jr.', email: 'ironman@starc.com' },
+    { id: 7, name: 'Robert Downy Sr.', email: 'ironmandad@starc.com' },
+    { id: 8, name: 'Robert Downy Jr.', email: 'ironman@starc.com' },
 ];
 const columnFirstTable = [
-    { label: 'Name', type:'fc_CustomCell',initialWidth: 200, typeAttributes :{value : { fieldName: 'name' }}},
-    { label: 'Email', type:'fc_CustomCell', initialWidth: 200, typeAttributes :{value : { fieldName: 'email' }} }
+    { label: 'Name', type: 'fc_CustomCell', initialWidth: 200, typeAttributes: { value: { fieldName: 'name' } } },
+    { label: 'Email', type: 'fc_CustomCell', initialWidth: 200, typeAttributes: { value: { fieldName: 'email' } } }
 ];
 
 const dataSecondTable = [
-    { id: 1, age: 40,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452'},
-    { id: 2, age: 35,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
-    { id: 3,age: 50,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
-    { id: 4,age: 37,address:  '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452'},
-    { id: 5, age: 40,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452'},
-    { id: 6, age: 35,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
-    { id: 7,age: 50,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
-    { id: 8,age: 37,address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452'},
+    { id: 1, age: 40, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 2, age: 35, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 3, age: 50, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 4, age: 37, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 5, age: 40, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 6, age: 35, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 7, age: 50, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
+    { id: 8, age: 37, address: '107, Parkland avenue,near Mayfair Hotel,across Newlane street, London - 4521452' },
 ];
 
 const columnsSecondTable = [
@@ -50,7 +50,16 @@ export default class fixedColumnedDatatable extends LightningElement {
     sortedBy;
     dataFirstTable = dataFirstTable;
 
-    // Used to sort the 'Age' column
+    onHandleSort(event) {
+        const { fieldName: sortedBy, sortDirection } = event.detail;
+        const cloneData = [...this.dataSecondTable];
+
+        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+        this.dataSecondTable = cloneData;
+        this.sortDirection = sortDirection;
+        this.sortedBy = sortedBy;
+    }
+
     sortBy(field, reverse, primer) {
         const key = primer
             ? function (x) {
@@ -65,15 +74,5 @@ export default class fixedColumnedDatatable extends LightningElement {
             b = key(b);
             return reverse * ((a > b) - (b > a));
         };
-    }
-
-    onHandleSort(event) {
-        const { fieldName: sortedBy, sortDirection } = event.detail;
-        const cloneData = [...this.dataSecondTable];
-
-        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
-        this.dataSecondTable = cloneData;
-        this.sortDirection = sortDirection;
-        this.sortedBy = sortedBy;
     }
 }
